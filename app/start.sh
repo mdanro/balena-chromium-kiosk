a2aaa78cbc23fbbe96c1e5d04221fee7661e1db1
DANIEL MARIN <mdanro@gmail.com>
2019-03-10 22:38:32 +0200

    triggers to fix chrome startup

--- app/start.sh
+++ app/start.sh
@@ -18,9 +18,37 @@ sed -i -e 's/console/anybody/g' /etc/X11/Xwrapper.config
 useradd chromium -m -s /bin/bash -G root
 usermod -a -G root,tty chromium
 
+export DISPLAY=:0.0
+
+# Start cursor at the top-left corner, as opposed to the default of dead-center
+# (so it doesn't accidentally trigger hover styles on elements on the page)
+xdotool mousemove 0 0
+
+# Set some useful X preferences
+xset s off # don't activate screensaver
+xset -dpms # disable DPMS (Energy Star) features.
+xset s noblank # don't blank the video device
+
+# Set X screen background
+sudo nitrogen --set-centered background.png
+
+# Hide cursor afer 5 seconds of inactivity
+unclutter -idle 5 -root &
+
+# Make sure Chromium profile is marked clean, even if it crashed
+if [ -f .config/chromium/Default/Preferences ]; then
+    cat .config/chromium/Default/Preferences \
+        | jq '.profile.exit_type = "SessionEnded" | .profile.exited_cleanly = true' \
+        > .config/chromium/Default/Preferences-clean
+    mv .config/chromium/Default/Preferences{-clean,}
+fi
+
+# Remove notes of previous sessions, if any
+find .config/chromium/ -name "Last *" | xargs rm
+
 # adding script to start chromium
 echo "#!/bin/bash" > /home/chromium/xstart.sh
-echo "chromium-browser --kiosk $URL_LAUNCHER_URL --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage" >> /home/chromium/xstart.sh
+echo "chromium-browser --start-fullscreen --window-size=1920,1080 --disable-infobars --kiosk $URL_LAUNCHER_URL --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage" >> /home/chromium/xstart.sh
 chmod 770 /home/chromium/xstart.sh
 chown chromium:chromium /home/chromium/xstart.sh
 
